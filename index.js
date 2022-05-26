4c226773d26f320c8e645e793407478d6d7cf694
Darobinson20 <Darobinson20@gmail.com>
2022-05-26 01:22:07 -0400

    trying to get table to show

--- index.js
+++ index.js
@@ -4,19 +4,24 @@ const inquirer = require('inquirer');
 const cTable = require('console.table');
 
 
-// Connect to database
-const db = mysql.createConnection(
+
+// Create connection to database
+const connection = mysql.createConnection(
     {
       host: 'localhost',
-      // MySQL username,
       user: 'root',
-      // MySQL password
       password: 'Kakashi314*',
       database: 'employee_db'
     },
     console.log(`Connected to the employee_db database.`)
   );
 
+  connection.connect(function(err){
+    if (err) throw err;
+
+  })
+  
+  //begin prompt to present options
   const promptUser= () =>{
 
   inquirer.prompt ([
@@ -28,9 +33,37 @@ const db = mysql.createConnection(
         }
     
      
-    ]);
-}
+    ])
+    .then((answer) => {
+      console.log(answer);
+      switch (answer.choice) {
+        case "View All Employees":
+          return viewAllEmployees();
+        case "Add Employee":
+          return addEmployee();
+        case "Update Employee Role":
+          return updateEmployeeRole();
+        case "View All Roles":
+          return viewAllRoles();
+        case "View All Departments":
+          return viewAllDepartments();
+        case "Add Department":
+          return addDepartment(); 
+        case "Add Role":
+          return addRole();          
+      }
+    });
+  }  
 
+function viewAllEmployees() {
+  var query = 'SELECT * FROM employee';
+  connection.query(query), function(err, res) {
+    if (err) throw err;
+    console.log(res.length + 'employees');
+    console.table(' View All Employees', res);
+    promptUser();
+  }
+}
 
 
 
