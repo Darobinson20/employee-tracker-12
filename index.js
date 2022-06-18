db80b593cb84d401d8fd80ca371c154ea45a498c
Darobinson20 <Darobinson20@gmail.com>
2022-06-18 14:48:01 -0400

    trying to get add employee to work

--- index.js
+++ index.js
@@ -59,13 +59,84 @@ function viewAllEmployees() {
   var query = 'SELECT * FROM employee';
   connection.query(query, (err, res) => {
     if (err) throw err;
-    
+
     console.log(res.length + 'employees');
     console.table(' View All Employees', res);
     promptUser();
   })
 }
 
+function viewAllRoles() {
+  var query = 'SELECT * FROM roles';
+  connection.query(query, (err, res) => {
+    if (err) throw err;
+    
+    console.log(res.length + 'roles');
+    console.table('View All Roles', res);
+    promptUser();
+  })
+}
 
+
+function viewAllDepartments() {
+  var query = 'SELECT * FROM department';
+  connection.query(query, (err, res) => {
+    if (err) throw err;
+    
+    console.log(res.length + 'department');
+    console.table('View All Departments', res);
+    promptUser();
+  })
+}
+
+//create prompts too add employee
+addEmployee = () => {
+  inquirer.prompt([
+    {
+      type: 'input',
+      name: 'first_name',
+      message: 'What is employees first name?',
+    
+    },
+    {   
+    type: 'input',
+      name: 'last_name',
+      message: 'What is employees last name?',
+    }
+    .then(res => {
+      var first_name = res.first_name;
+      var last_name = res.last_name;
+
+      db.findAllRoles()
+      .then(([rows]) => {
+        var roles = rows;
+        const rolesChoices = roles.map(({ id, title }) => ({
+          name: title,
+          value: id
+        }));
+
+        promptUser({
+          type: 'list',
+          name: 'rolesId',
+          message: 'What is the employees role?',
+          choices: rolesChoices
+        })
+        .then(res => {
+          var rolesId = res.rolesId;
+
+          db.findAllEmployee()
+          .then(([rows]) => {
+            var employee = rows;
+            const managerChoices = employee.map (({ id, first_name, last_name }) => ({
+              name: `${first_name} ${last_name}`,
+              value: id
+            }))
+          })
+        })
+      })
+    })
+
+  ])
+}
 
 promptUser();
